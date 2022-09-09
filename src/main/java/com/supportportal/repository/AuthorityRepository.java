2a6ee113397fdf6f9a636e14e98df440a7b4189f
Oket Hoxha <oket.hoxha@ad.syncpilot.com>
2022-09-10 00:13:51 +0200

    Initial Commit!

---
+++ src/main/java/com/supportportal/repository/AuthorityRepository.java
@@ -0,0 +1,10 @@
+package com.supportportal.repository;
+
+import com.supportportal.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AuthorityRepository extends JpaRepository<Authority, Integer> {
+    List<Authority> findAllByStatusIs(Integer status);
+}
